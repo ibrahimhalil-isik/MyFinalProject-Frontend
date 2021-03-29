2661502aaa49b4d3dca2653fe078ea4d78c34dac
qubra <qubra@DESKTOP-VEOETUV>
2021-03-29 23:36:56 +0300

    Kodlama.io

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
+  categories: Category[] = [];
+  dataLoaded = false;
 
-  constructor() { }
+  constructor(private CategoryService:CategoryService) { }
 
   ngOnInit(): void {
+    this.getCategories();
   }
 
+  getCategories() {
+    this.CategoryService.getCategories().subscribe(response=>{
+      this.categories = response.data
+      this.dataLoaded = true;
+    })
+  }
 }
