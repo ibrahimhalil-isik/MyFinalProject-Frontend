1e6b7dc7179873ce676c9903a9033bae0d937c51
qubra <qubra@DESKTOP-VEOETUV>
2021-04-05 18:28:52 +0300

    Kodlama.io

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
 import { ListResponseModel } from '../models/listResponseModel';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -24,5 +25,9 @@ export class ProductService {
   getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>> {
     let newPath = this.apiUrl + "products/getbycategoryid?categoryId="+categoryId
     return this.httpClient.get<ListResponseModel<Product>>(newPath)     
+  }
+
+  add(product:Product):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"products/add",product)
   }
 }
